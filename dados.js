b9c90befd1460740ecf79ca46fc02352b7360a75
MateusKaio <mateuskaio819@gmail.com>
2024-09-08 17:17:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Asuma",
+        descricao: "Asuna Yuuki é uma das protagonistas de Sword Art Online. Conhecida como a Espadachim Flash, ela é uma habilidosa duelista e ex-líder de um guild. Fria e calculista à primeira vista, Asuna revela um lado gentil e protetor ao lado de Kirito.",
+        link: "https://en.wikipedia.org/wiki/Asuna_Yuuki",
+        tags: "esposa do protagonista"
+    },
+    {
+        titulo: "Kirito",
+        descricao: "Kirito, cujo nome real é Kazuto Kirigaya, é o protagonista da série Sword Art Online. Conhecido como o Espadachim Negro, ele é um jogador habilidoso e estrategista, famoso por suas habilidades de combate e por sua personalidade solitária. Preso no mortal jogo de realidade virtual Sword Art Online, Kirito luta para sobreviver e encontrar uma saída, enquanto desenvolve fortes laços de amizade com outros jogadores, especialmente com Asuna.",
+        link: "https://en.wikipedia.org/wiki/Kirito_(Sword_Art_Online)",
+        tags: "o mais forte"
+    },
+    {titulo: "Klein",
+        descricao: "Klein, cujo nome real é Yui Kirigaya, é um dos primeiros amigos de Kirito em Sword Art Online. Conhecido por sua personalidade amigável e lealdade, ele é um guerreiro habilidoso e um companheiro confiável.",
+        link: "https://swordartroom.wixsite.com/sword-art-online/klein",
+        tags: "Amigo do protagonista"
+    }
+];
+
